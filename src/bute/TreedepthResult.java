957efaab762dcf139d92abc8308da96dec617cb6
James Trimble <james.trimble@yahoo.co.uk>
2021-01-04 12:02:47 +0000

    Remove unused satisfied field of TreedepthResult

--- src/bute/TreedepthResult.java
+++ src/bute/TreedepthResult.java
@@ -1,22 +1,12 @@
 package bute;
 
 class TreedepthResult {
-    private final boolean satisfied;
     private int depth;
     private int[] parent;
 
-    TreedepthResult() {
-        satisfied = false;
-    }
-
     TreedepthResult(int depth, int[] parent) {
-        satisfied = true;
         this.depth = depth;
         this.parent = parent;
-    }
-
-    boolean isSatisfied() {
-        return satisfied;
     }
 
     int getDepth() {
